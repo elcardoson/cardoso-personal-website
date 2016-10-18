2701422d60c5d0636ef599a709ad97b116431688
Tiago <hello@cardoso.cc>
2016-10-18 08:45:49 -0200

    html, css adjustments

--- gulpfile.js
+++ gulpfile.js
@@ -7,40 +7,43 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var concat = require('gulp-concat');
 var less = require('gulp-less');
+var cleanCSS = require('gulp-clean-css');
 var path = require('path');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['html', 'css'], function() {
+gulp.task('serve', ['html', 'minify-css'], function() {
 
     browserSync.init({
          server: "./app"
     });
-    gulp.watch("./less/*.less").on('change', browserSync.reload);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
-    gulp.watch("app/styles/*.css").on('change', browserSync.reload);
-    gulp.watch("app/scripts/*.js").on('change', browserSync.reload);
+   // gulp.watch("./less/*.less").on('change', browserSync.reload);
+    gulp.watch("./less/*.less", ['minify-css']);
+    gulp.watch("./app/*.html").on('change', browserSync.reload);
+    gulp.watch("./app/scripts/*.js").on('change', browserSync.reload);
 });
 
 
 gulp.task('html', function() {
-    return gulp.src("app/*.html")
+    return gulp.src("./app/*.html")
         .pipe(browserSync.stream());
 });
 
-gulp.task('css', function() {
-    return gulp.src("app/css/*.css")
-        .pipe(browserSync.stream());
-});
 
 gulp.task('less', function () {
-  return gulp.src('./less/**/*.less')
+  return gulp.src('./less/styles.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    .pipe(gulp.dest('./app/styles'));
+    .pipe(gulp.dest('./app/css'))
     .pipe(browserSync.stream());
 });
 
+gulp.task('minify-css', ['less'], function() {
+  return gulp.src('./app/css/styles.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./app/css/'))
+    .pipe(browserSync.stream());
+});
 
  // Concatenate JS Files
 gulp.task('scripts', function() {
@@ -51,4 +54,4 @@ gulp.task('scripts', function() {
 
 
  // Default Task
-gulp.task('default', ['serve','scripts', 'less']);
+gulp.task('default', ['serve','scripts']);
